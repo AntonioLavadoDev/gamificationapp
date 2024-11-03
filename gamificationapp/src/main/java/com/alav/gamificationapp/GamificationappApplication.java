b0aac1ef0203b1f7b0efd9f7155cf1bef9bcc125
Antonio Lav <antoniolavadodev@gmail.com>
2024-11-03 11:20:52 +0100

    feat: initial project structure setup

---
+++ gamificationapp/src/main/java/com/alav/gamificationapp/GamificationappApplication.java
@@ -0,0 +1,13 @@
+package com.alav.gamificationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamificationappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamificationappApplication.class, args);
+	}
+
+}
---
+++ gamificationapp/src/test/java/com/alav/gamificationapp/GamificationappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alav.gamificationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamificationappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
